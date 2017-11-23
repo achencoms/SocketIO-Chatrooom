ecdabb6d4f0daa6db9593fd7ac77cf6de3a66ff5
achencoms <achencoms@github.com>
2017-11-22 23:17:23 -0500

    added remove rooms

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room, rooms
 
-import os
+import os, json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32);
 socketio = SocketIO(app)
+users = []
 
 @app.route('/')
 def home():
-    return render_template('enter.html')
+    if 'user' in session:
+        return redirect(url_for("chat"))
+    return render_template("enter.html")
 
 @app.route('/enter', methods=['POST'])
 def index():
     d = request.form
-    session['user'] = d["user"]
-    return redirect(url_for("chat"))
+    user = d['user']
+    if 'user' in session:
+        return redirect(url_for("chat"))
+    if user not in users:
+        users.append(user)
+        session['user'] = user
+        return redirect(url_for("chat"))
+    return redirect(url_for("home"))
 
 @app.route('/chatroom')
 def chat():
@@ -23,33 +32,37 @@ def chat():
         return redirect(url_for("home"))
     return render_template('index.html')
 
+@app.route("/checkName", methods=["GET"])
+def checkName():
+    d = request.args
+    name = d['name']
+    out = {"check" : name in users}
+    return json.dumps(out)
+
 @socketio.on('message', namespace="/chatroom")
 def handle_message(message):
-    if len(rooms()) == 2:
-        emit('userMessage',{'m' : message, 'user' : session['user']},room="general")
-    else:
-        for i in rooms():
-            if i != request.sid:
-                emit('userMessage',{'m' : message, 'user' : session['user']},room = i)
+    emit('userMessage',{'m' : message['m'], 'user' : session['user']}, room=message['room'])
 
 @socketio.on('connect', namespace="/chatroom")
 def test_connect():
-    print "client has connected\n\n"
+    print ("client has connected\n\n")
     join_room("general")
 
 @socketio.on('join', namespace="/chatroom")
 def on_join(data):
     room = data['room']
-    join_room(room)
-    send("Your current rooms are: " + str(rooms()))
-    send(session['user'] + " has joined " + room + ".", room = room)
-    emit("room_check", {
-        "rooms" : [room for room in rooms() if room != request.sid]
-    })
+    if(room not in rooms()):
+        join_room(room)
+        roomList = [room for room in rooms() if room != request.sid]
+        emit("room_check", {
+            "rooms" : roomList
+        })
+        send("Your current rooms are: " + str(roomList))
+        emit("message", {'m': session['user'] + " has joined " + room + ".", 'room' : room}, room = room)
 
 @socketio.on('leave')
 def on_leave(data):
-    username = data['username']
+    username = session['user']
     room = data['room']
     leave_room(room)
     send(username + ' has left the room.', room=room)
@@ -58,10 +71,6 @@ def on_leave(data):
 @socketio.on('my broadcast event')
 def test_message(message):
     emit('my response', {'data': message['data']}, broadcast=True)
-
-@socketio.on('connect')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
 
 @socketio.on('disconnect')
 def test_disconnect():
